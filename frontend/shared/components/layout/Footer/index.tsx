11b2bc634439db568ec9112d18d972a6c5fe4f7c
d1av <davi4alves@gmail.com>
2022-12-03 21:40:12 -0300

    feat: add footer

--- frontend/shared/components/layout/Footer/index.tsx
+++ frontend/shared/components/layout/Footer/index.tsx
@@ -1,6 +1,16 @@
 const Footer = () => {
   return (
-    <footer>Footer</footer>
+    <footer className="footerContainer">
+      <div className="copyrightText">
+        Copyright 2022 - Julius Team
+      </div>
+      <div className="iconsContainer">
+        <div className="icon">1</div>
+        <div className="icon">2</div>
+        <div className="icon">3</div>
+        <div className="icon">4</div>
+      </div>
+    </footer>
   )
 }
 
