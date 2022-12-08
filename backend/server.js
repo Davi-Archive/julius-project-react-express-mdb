4b711482d6113cf1b9e4767179c7e51e24cb42ec
d1av <davi4alves@gmail.com>
2022-12-08 12:38:12 -0300

    feat: add swagger doc, for better communication

--- backend/server.js
+++ backend/server.js
@@ -3,20 +3,25 @@ const dotenv = require("dotenv").config({ path: "./.env" });
 const colors = require("colors");
 var cors = require("cors");
 const path = require("path");
+const swaggerUi = require("swagger-ui-express");
+const swaggerFile = require("./swagger/swagger_output.json");
+
 
 const connectDB = require("./config/db.js");
 
 const app = express();
 
+app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-//index of the API
-app.get("/", (req, res) => {
-  res.status(200)
-  res.sendFile(path.join(__dirname, "./views/index.html"));
-});
+// //index of the API
+// app.get("/", (req, res) => {
+//   res.status(200)
+//   res.sendFile(path.join(__dirname, "./views/index.html"));
+// });
 
 //about section route
 app.use("/api/post", require("./routes/postRoutes.js"));
